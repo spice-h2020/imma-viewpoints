2e28f7a344fb28c3f47cdc86fc7cbffb0a09e885
Jason Carvalho <jase@jasoncarvalho.com>
2021-05-20 15:08:30 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'imma-viewpoints';
+  title = 'IMMA Viewpoints';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtworksComponent } from './artworks/artworks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtworksComponent
   ],
   imports: [
     BrowserModule,
