3acc280542f374d8c26559b344a92ce59b914286
Jason Carvalho <jase@jasoncarvalho.com>
2021-06-15 13:59:09 +0100

    Progress bar for loading initial list of artworks

--- src/app/artworks/artworks.component.ts
+++ src/app/artworks/artworks.component.ts
@@ -9,16 +9,21 @@ import { ArtworkService } from '../artwork.service';
 })
 export class ArtworksComponent implements OnInit {
   artworks: Artwork[] = [];
+  artworksLoaded: boolean;
 
   constructor(private artworksService: ArtworkService) { }
 
   ngOnInit(): void {
+    this.artworksLoaded = false;
     this.getArtworks();
   }
 
   getArtworks(): void {
     this.artworksService.getArtworks()
-      .subscribe(artworks => this.artworks = artworks);
+      .subscribe(artworks => {
+        this.artworks = artworks;
+        this.artworksLoaded = true;
+      });
   }
 
 }
