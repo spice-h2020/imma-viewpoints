849d6727fcea3ae2a9809e043a3546643f15aa0d
Jason Carvalho <jase@jasoncarvalho.com>
2021-06-15 21:29:13 +0100

    Add results page (and component)

--- src/app/artwork.service.ts
+++ src/app/artwork.service.ts
@@ -13,45 +13,6 @@ export class ArtworkService {
 
   constructor(private http: HttpClient) { }
 
-  private generateDummyArtworks(): Artwork[] {
-    const artworks: Artwork[] = [
-      {
-        _id: '1001',
-        name: '8 Limestones',
-        artist: 'Ulrich Rückriem',
-        date: new Date(),
-        description: 'Artwork description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        imageLoc: './assets/img/img1.jpg'
-      },
-      {
-        _id: '1002',
-        name: '217 5° Arc x 12',
-        artist: 'Benar Venet',
-        date: new Date(),
-        description: 'Artwork description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        imageLoc: './assets/img/img2.jpg'
-      },
-      {
-        _id: '1003',
-        name: 'Barrel',
-        artist: 'Fergus Martin',
-        date: new Date(),
-        description: 'Artwork description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        imageLoc: './assets/img/img3.jpg'
-      },
-      {
-        _id: '1004',
-        name: 'The Drummer',
-        artist: 'Barry Flanagan',
-        date: new Date(),
-        description: 'Artwork description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        imageLoc: './assets/img/img4.jpg'
-      }
-    ];
-    return artworks;
-
-  }
-
   getArtworks(): Observable<Artwork[]> {
     const artworksPath = '?action=artworklist';
     return this.http.get<Artwork[]>(this.apiUrl + artworksPath);
--- src/app/response.service.ts
+++ src/app/response.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Response } from './response';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
